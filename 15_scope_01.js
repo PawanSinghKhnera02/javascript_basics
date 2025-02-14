a70d0702225b7910df79ef2e253ce91caa433836
Pawan singh khnera <pawansingh.khnera25@gmail.com>
2025-02-14 14:05:29 +0530

    function

---
+++ 15_scope_01.js
@@ -0,0 +1,50 @@
+// var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // var c = 30
+
+    console.log("inner:", a);
+}
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+// ======================== Nested Scope===============================
+
+function one() {
+    const username= "pawan";
+    function two() {
+        const website = "youtube";
+        console.log(username);
+    }
+    // console.log(website);
+    two();
+}
+// one();
+
+if (true) {
+    const username = "pawan";
+    if(username === "pawan") {
+        const website = "youtube"
+        console.log(username + " " + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+// ======================= Interesting==============================
+console.log(addone(5)); // it runs perfectly fine 
+function addone(num) {
+    return num + 1
+}
+addone(5);
+
+// console.log(addtwo(5)); // it gives error if we try to console before function declaration with assigning to variable
+const addtwo = function (num) {
+    return num + 2
+}
+addtwo(5);
---
+++ 16_Arrow_this_keyword.js
@@ -0,0 +1,56 @@
+const user = {
+    username: 'pawan',
+    price: 199,
+
+    welcomeMessage : function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+}
+// user.welcomeMessage();
+// user.username = "sam";
+// user.welcomeMessage();
+
+// console.log(this); //this will give empty parenthesis cause in node environment this refer empty object while browser gives (window object - referred as global object in browser)
+
+
+function chai () {
+    let username = "pawan";
+    console.log(this.username); // this won't work inside function only works inside objects like above
+}
+// chai();
+
+const tea = function () {
+    let username = "pawan";
+    console.log(this.username); // this won't work inside function only works inside objects like above
+}
+// tea(); //gives undefined 
+
+const coffee =  () => {
+    let username = "pawan";
+    console.log(this.username); // this won't work inside function only works inside objects like above
+}
+// coffee(); 
+
+
+// =================== Arrow function ====================
+//basc arrow funtion
+const addtwo = (num1, num2) => {
+    return num1 + num2
+}
+console.log(addtwo(2,3));
+
+
+//implicit return //doesnt requied return keyword
+const addTwo = (num1, num2) => num1 + num2
+const AddTwo = (num1, num2) => ({username: "pawan"})  //object return requires square bracket
+const addthree = (num1, num2) => (num1 + num2 + num3)
+
+console.log(addTwo(5,3));
+console.log(AddTwo(5,3));
+console.log(addTwo(4,1,3));
+
+const myArray = [2,5,3,7,8]
+myArray.forEach( function (){}) ;
+myArray.forEach(() => {});
+
